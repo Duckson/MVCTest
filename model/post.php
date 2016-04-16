bd4f57cd95f45a13328a482058d82650d2dfe9b4
Duckson <vladproff@gmail.com>
2016-04-16 20:42:58 +0300

    Sidebar MVC update

--- model/post.php
+++ model/post.php
@@ -1,20 +1,24 @@
 <?php
-class Post{
+
+class Post
+{
     public $title;
     public $text;
     // Это статический метод. Он вызывается без создание объекта
     // Вызывается вот так Класс::метод()
     // т.е. Post::getAll()
-    public static function getAll(){
+    public static function getAll()
+    {
         $obj_array = [];
-        foreach ($_SESSION['news'] as $news){
+        foreach ($_SESSION['news'] as $news) {
             $post_obj = new Post();
-            $post_obj->title=$news['title'];
-            $post_obj->text=$news['text'];
-            $obj_array[]=$post_obj;
+            $post_obj->title = $news['title'];
+            $post_obj->text = $news['text'];
+            $obj_array[] = $post_obj;
         }
         return $obj_array; // возвращаем массив объектов
     }
+
     public static function writePost()
     {
         if (!isset($_SESSION['news'])) $_SESSION['news'] = [];
@@ -23,8 +27,8 @@ class Post{
 
         if (!empty($title) && !empty($text))
             $_SESSION['news'][] = [
-            'title' => $title,
-            'text' => $text
-        ];
+                'title' => $title,
+                'text' => $text
+            ];
     }
 }
--- views/sidebar.php
+++ views/sidebar.php
@@ -1,8 +1,8 @@
 <div class="col-sm-2 sidenav">
     <h4>Новости</h4>
     <ul class="nav nav-pills nav-stacked">
-        <? foreach ($_SESSION['news'] as $news): ?>
-            <li><?= $news['title'] ?></li>
+        <? foreach (Post::getAll() as $news): ?>
+            <li><?= $news->title ?></li>
         <? endforeach ?>
     </ul>
     <br>
